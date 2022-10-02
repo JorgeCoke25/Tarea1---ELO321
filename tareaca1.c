59ad620cdd5c3cff8605de60f08983f8e68217d4
Jorge_Barrios <jorge.barriosn@usm.cl>
2022-10-02 16:30:58 -0300

    Avance con el pipe

--- tareaca1.c
+++ tareaca1.c
@@ -21,7 +21,7 @@ void vaciar(char temp[]){
   }
 }
 
-void copiar(char temp[], int i){
+void copiar(char* temp, int i){
   int N = strlen(temp)+1;
   prod[i].nombre = (char*)malloc(N*sizeof(char));
   strcpy(prod[i].nombre,temp);
@@ -29,7 +29,18 @@ void copiar(char temp[], int i){
 void copiarPrecio(char temp[], int i){
   //prod[i].precio = (int)malloc(sizeof(int));
   prod[i].precio=atoi(temp);
-} 
+}
+//Obtiene una id a partir del nombre del producto
+int obtenerId(char* str){
+    int i =0;
+    while (i<100){
+        if(strcmp(str,prod[i].nombre)==0){
+            return prod[i].id;
+        }
+        i++;
+    }
+    return 0;
+}
 /////////////////
 int main(){
 
@@ -51,37 +62,36 @@ int main(){
     }
     rewind(inventario);
     prod = (producto*)malloc(cont*sizeof(producto));
-    
+
     int i,j,a=0;
     for(i = 0; !feof(inventario) ; i++){
       vaciar(temp);
-      aux = '0';
+      char aux;
       for(j = 0; a%2!=1; j++){
         aux=fgetc(inventario);
         if(aux==',')a++;
-        if(a!=1){
+        else if(a!=1){
           temp[j]=aux;
         }
       }
       copiar(temp,i);
       //almacenando precios      printf("%d\nxd",atoi(temp));
 
-      
       for(j=0;a%2!=0;j++){
 
         aux=fgetc(inventario);
         if(aux==',')a++;
-        if(a!=2){
+        else if(a!=2){
           temp[j]=aux;
         }
       }
       copiarPrecio(temp,i);
-      
-      
+
+
       /*fgets(temp, 6, inventario);//6 caracteres que puede llegar a leer de precio
       prod[i].precio = atoi(temp);*/
       //fin de almacenar los precios
-      fgets(temp,4, inventario);
+      fgets(temp,3, inventario);
       prod[i].id = atoi(temp);
       fgets(temp,10, inventario);
       prod[i].tiempo = atoi(temp);
@@ -90,26 +100,29 @@ int main(){
     }
     // Fin de almacenar el txt en la estructura
     char strLine[50];
-    char* buffer[100];
+    int buffer[100];
     //Lectura de los pedidos para guardarlos en el buffer
-    cont = 0;
+    cont = 0;//contador que marca la posicion en el buffer
     while (fgets(strLine, 50, pedidos)){
+        //Aca se elimina el ultimo caracter "\n" de la linea
+        strLine[strlen(strLine)-1]='\0';
+        //Se toma el primer producto de el pedido
         char *token = strtok(strLine, ",");
-        buffer[cont]=token;
+        //Se pasa al buffer el id del primer producto
+        buffer[cont]=obtenerId(token);
         cont++;
         if(token != NULL){
             while(token != NULL){
                 // Sólo en la primera pasamos la cadena; en las siguientes pasamos NULL
-                token = strtok(NULL, ",");
-                buffer[cont]=token;
-                cont++;
+                buffer[cont]= obtenerId(token); //Se pasa la id del producto leido al buffer
+                cont++;//Se suma uno  a la posicion
+                token = strtok(NULL, ",");//Siguiente producto
             }
             close(tubo[0]);
-            write(tubo[1],buffer,sizeof(strLine));
+            write(tubo[1],buffer,cont*sizeof(int));
             wait(NULL);
             cont=0;
         }
-        printf("%s\n",buffer[0]);
     }
     pid_t repartidor1 = fork();  //inicializando hijo 1
     if(repartidor1<0){
@@ -117,18 +130,18 @@ int main(){
         return 1;
     }else if(repartidor1==0){ //proceso hijo 1
         printf("soy el repartidor 1 jojojo\n");
-        char* buffer[1];//Variable de lectura
+        int buff[1];//Variable de lectura
         close(tubo[1]);//Se cierra el modo escritura
         int i =0;
         while (1){
-            read(tubo[0],buffer,8);//Modo de lectura 1 palabra
+            read(tubo[0],buff, sizeof(int));//Modo de lectura 1 entero
             i++;
-            if(i==100 || buffer[0]==NULL){
+            if(i==10 || buff[0]==0){
                 break;
             }
-            printf("Buffer: %s - Iteracion numero: %d\n",buffer[0],i);
+            printf("Buffer: %d - Iteracion numero: %d\n",buffer[0],i);
         }
-        
+
     }
 
     repartidor2 = fork();  //inicializando hijo 2
