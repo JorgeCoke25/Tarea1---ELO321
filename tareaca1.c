2bb145e361e6de6c7545337aa57b705c52773e0f
Jorge_Barrios <jorge.barriosn@usm.cl>
2022-10-03 18:13:28 -0300

    Avance del codigo

--- tareaca1.c
+++ tareaca1.c
@@ -13,6 +13,10 @@ typedef struct{
 } producto;
 
 producto *prod;
+
+int total=0; //Puntero que almacena el precio total del dia
+
+
 //funciones de utilidad
 void vaciar(char temp[]){
   int i;
@@ -50,21 +54,27 @@ int main(){
     FILE *inventario, *pedidos; // procedimiento Cajero
     inventario = fopen("inventario.txt", "r");
     pedidos = fopen("pedidos.txt","r");
-    
+
+
     // Almacenando el archivo de texto en la estructura producto
     char temp[50];
     int cont=0;
+    //Se cuentan las lineas del archivo
     while(!feof(inventario)){
       fgets(temp,50,inventario);
       cont++;
     }
+    //Se vuelve al princio del archivo
     rewind(inventario);
+    //Se reserva memoria para el arreglo de productos
     prod = (producto*)malloc(cont*sizeof(producto));
 
-    int i,j,a=0;
+    int i,j,a=0; // variable "i" marca la posicion de el producto en el arreglo, j la posicion del caracter para armar el string y "a" el contador que si es "1" es nombre y "2" si es el precio
+    //Ciclo for hasta que se termine de leer el archivo
     for(i = 0; !feof(inventario) ; i++){
       vaciar(temp);
       char aux;
+      //Ciclo for hasta que se encuentra el delimitador ","
       for(j = 0; a%2!=1; j++){
         aux=fgetc(inventario);
         if(aux==',')a++;
@@ -72,61 +82,62 @@ int main(){
           temp[j]=aux;
         }
       }
+      //Se almacena el nombre de la structura en la posicion "i"
       copiar(temp,i);
-      //almacenando precios      printf("%d\nxd",atoi(temp));
 
+      //Ciclo for hasta que se encuentra el delimitador ","
       for(j=0;a%2!=0;j++){
-
         aux=fgetc(inventario);
         if(aux==',')a++;
         else if(a!=2){
           temp[j]=aux;
         }
       }
+      //Se almacena el precio de la structura en la posicion "i"
       copiarPrecio(temp,i);
 
-
-      /*fgets(temp, 6, inventario);//6 caracteres que puede llegar a leer de precio
-      prod[i].precio = atoi(temp);*/
       //fin de almacenar los precios
       fgets(temp,3, inventario);
+      //Se almacena la id de la structura en la posicion "i"
       prod[i].id = atoi(temp);
       fgets(temp,10, inventario);
+      //Se almacena el tiempo de la structura en la posicion "i"
       prod[i].tiempo = atoi(temp);
       printf("Nombre producto: %s Precio: %d ID: %d Tiempo de elaboracion: %d \n",prod[i].nombre,prod[i].precio, prod[i].id, prod[i].tiempo);
-
     }
     // Fin de almacenar el txt en la estructura
-
-    repartidor1 = fork();  //inicializando hijo 1
-    if(repartidor1>0){
-        char strLine[50];
-        //Lectura de los pedidos para guardarlos en el buffer
-        cont = 0;//contador que marca la posicion en el buffer
-        while (fgets(strLine, 50, pedidos)){
-            int buffer[100];
-            //Aca se elimina el ultimo caracter "\n" de la linea
-            strLine[strlen(strLine)-1]='\0';
-            //Se toma el primer producto de el pedido
-            char *token = strtok(strLine, ",");
-            if(token != NULL){
-                while(token != NULL){
-                    // Sólo en la primera pasamos la cadena; en las siguientes pasamos NULL
-                    buffer[cont]= obtenerId(token); //Se pasa la id del producto leido al buffer
-                    cont++;//Se suma uno  a la posicion
-                    token = strtok(NULL, ",");//Siguiente producto
-                }
-                buffer[cont]=0;
-                write(tubo[1],buffer,sizeof(int)*100);
-                close(tubo[1]);//Se cierra modo escritura
-                wait(NULL);
-                cont=0;
+    char strLine[50];
+    //Lectura de los pedidos para guardarlos en el buffer
+    cont = 0;//contador que marca la posicion en el buffer
+    char *lectura = fgets(strLine, 50, pedidos);//Variable que confirma si se estan leyendo pedidos
+    while (lectura){
+        int buffer[100];
+        //Aca se elimina el ultimo caracter "\n" de la linea
+        strLine[strlen(strLine)-1]='\0';
+        //Se toma el primer producto de el pedido
+        char *token = strtok(strLine, ",");
+        if(token != NULL){
+            while(token != NULL){
+                // Sólo en la primera pasamos la cadena; en las siguientes pasamos NULL
+                buffer[cont]= obtenerId(token); //Se pasa la id del producto leido al buffer
+                cont++;//Se suma uno  a la posicion
+                token = strtok(NULL, ",");//Siguiente producto
             }
+            buffer[cont]=0;
+            write(tubo[1],buffer,sizeof(int)*100);
+            wait(NULL);//Se espera a que el proceso hijo termine
+            cont=0;
         }
+        lectura = fgets(strLine, 50, pedidos);
+    }
+    repartidor1 = fork();  //inicializando hijo 1
+    if(repartidor1<0){
+        printf("Fallo en la creacion del hijo");
     }else if(repartidor1==0){ //proceso hijo 1
         printf("soy el repartidor 1 jojojo\n");
         int buff[1];//Variable de lectura
         close(tubo[1]);//Se cierra el modo escritura
+        int espera = 0; //Variable que definira el tiempo de preparacion total del pedido
         int i =0;
         while (1){
             read(tubo[0],buff,sizeof(int));//Modo lectura de 2 enteros
@@ -134,40 +145,22 @@ int main(){
             if(i==10 || buff[0]==0){
                 break;
             }
-            printf("Buffer: %d - Iteracion numero: %d\n",buff[0],i);
+            printf("Buffer: %d - Iteracion numero: %d - Repartidor 1\n",buff[0],i);
+            espera+=prod[buff[0]-1].tiempo;
+            total+=prod[buff[0]-1].precio;
         }
-        close(tubo[0]);
+        usleep(espera); //Tiempo de espera en micro segundos
+        close(tubo[0]);//Se cierra el modo lectura del tubo
     }
 
     repartidor2 = fork();  //inicializando hijo 2
-    if(repartidor2>0){
-        char strLine[50];
-        //Lectura de los pedidos para guardarlos en el buffer
-        cont = 0;//contador que marca la posicion en el buffer
-        while (fgets(strLine, 50, pedidos)){
-            int buffer[100];
-            //Aca se elimina el ultimo caracter "\n" de la linea
-            strLine[strlen(strLine)-1]='\0';
-            //Se toma el primer producto de el pedido
-            char *token = strtok(strLine, ",");
-            if(token != NULL){
-                while(token != NULL){
-                    // Sólo en la primera pasamos la cadena; en las siguientes pasamos NULL
-                    buffer[cont]= obtenerId(token); //Se pasa la id del producto leido al buffer
-                    cont++;//Se suma uno  a la posicion
-                    token = strtok(NULL, ",");//Siguiente producto
-                }
-                buffer[cont]=0;
-                write(tubo[1],buffer,sizeof(int)*100);
-                close(tubo[1]);//Se cierra modo escritura
-                wait(NULL);
-                cont=0;
-            }
-        }
+    if(repartidor2<0){
+        printf("Fallo la creacion del hijo");
     }else if(repartidor2==0){ //proceso hijo 2
         printf("soy el repartidor 2 jojojo\n");
         int buff[1];//Variable de lectura
         close(tubo[1]);//Se cierra el modo escritura
+        int espera = 0; //Variable que definira el tiempo de preparacion total del pedido
         int i =0;
         while (1){
             read(tubo[0],buff,sizeof(int));//Modo lectura de 2 enteros
@@ -175,11 +168,14 @@ int main(){
             if(i==10 || buff[0]==0){
                 break;
             }
-            printf("Buffer: %d - Iteracion numero: %d\n",buff[0],i);
+            printf("Buffer: %d - Iteracion numero: %d - Repartidor 2\n",buff[0],i);
+            espera+=prod[buff[0]-1].tiempo;
+            total+=prod[buff[0]-1].precio;
         }
+        usleep(espera);
         close(tubo[0]);
     }
-
+    printf("El total del día es : $%d\n",total);
     return 0;
 
 }
